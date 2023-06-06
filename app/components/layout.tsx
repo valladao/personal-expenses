ebe8de01400b1551a502273b8575a965d6ba9706
Manoel Valladao <valladao@gmail.com>
2023-06-05 21:37:26 -0300

    Basic layout styles

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -3,13 +3,13 @@ import ToggleSidebarButton from "./toggle-sidebar-button";
 
 export default function Layout() {
   return (
-    <>
-      <Sidebar></Sidebar>
-      <main>
-        <h1>Month Report</h1>
+    <div className="flex">
+      <Sidebar />
+      <main className="flex-1 p-4">
+        <h1 className="text-2xl font-bold mb-4">Month Report</h1>
         <p>This is the content for the Month Report page.</p>
-        <ToggleSidebarButton></ToggleSidebarButton>
+        <ToggleSidebarButton />
       </main>
-    </>
+    </div>
   )
 }
