d19b0df5269ee243fc719dae4fb6348e7dea656d
Manoel Valladao <valladao@gmail.com>
2023-06-14 19:15:35 -0300

    Improve sidebar

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,6 +1,24 @@
 import LinkList from "./link-list";
 import ToggleSidebarButton from "./toggle-sidebar-button";
 
+const entriesLinks = [
+  {
+    href: 'register-expenses.html',
+    innerText: 'Register Expenses'
+  },
+]
+
+const reportLinks = [
+  {
+    href: 'month-report.html',
+    innerText: 'Month Report'
+  },
+  {
+    href: 'account-report.html',
+    innerText: 'Account Report'
+  },
+]
+
 const databaseLinks = [
   {
     href: 'expense-group.html',
@@ -20,30 +38,17 @@ const databaseLinks = [
   },
 ]
 
-const reportLinks = [
-  {
-    href: 'month-report.html',
-    innerText: 'Month Report'
-  },
-  {
-    href: 'account-report.html',
-    innerText: 'Account Report'
-  },
-  {
-    href: 'register-expenses.html',
-    innerText: 'Register Expenses'
-  },
-]
-
 export type LinksListType = typeof databaseLinks;
 
 export default function Sidebar() {
   return (
     <dialog id="sidebar" className="w-64 bg-gray-200 p-4 m-0 h-full">
-      <div className="flex items-center">
-        <h1 className="text-xl font-bold mr-4">App Logo/Name</h1>
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold mr-4">Expenses App</h1>
         <ToggleSidebarButton text="&times;" />
       </div>
+
+      <LinkList linkArray={entriesLinks} listName='Entries'></LinkList>
 
       <LinkList linkArray={reportLinks} listName='Reports'></LinkList>
 
