b4bb589ab0cfdd8466e4368076341ebd36056a13
Manoel Valladao <valladao@gmail.com>
2023-06-15 12:02:57 -0300

    Create MonthReport route

---
+++ app/routes/month-report.tsx
@@ -0,0 +1,8 @@
+export default function MonthReport() {
+  return (
+    <div className="flex flex-col pl-4 text-zinc-800">
+      <h1 className="text-2xl font-bold">Month Report</h1>
+      <p>This is the content for the Month Report page.</p>
+    </div>
+  )
+}
