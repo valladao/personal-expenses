fc889cf2b23501cec5667220f1e3353920663d8d
Manoel Valladao <valladao@gmail.com>
2023-06-21 09:07:36 -0300

    Create AccountReport and RegisterExpenses

---
+++ app/routes/register-expenses.tsx
@@ -0,0 +1,10 @@
+import PageTitle from "~/components/page-title"
+
+export default function RegisterExpenses() {
+  return (
+    <div className="flex flex-col pl-4 text-zinc-800">
+      <PageTitle>Register Expenses</PageTitle>
+      <p>This is the content for the Register Expenses page.</p>
+    </div>
+  )
+}
