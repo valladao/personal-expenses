bc85ab21eb447b7fe11b58d0cc9000a239083abd
Manoel Valladao <valladao@gmail.com>
2023-09-11 20:30:36 -0300

    Fix full width issue

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({children}: {children: React.ReactNode}) {
   return (
     <div className="flex">
       <Sidebar />
-      <main id="main" className="flex p-4">
+      <main id="main" className="flex w-full p-4">
         <div className="flex">
           <ToggleSidebarButton text="&gt;" />
         </div>
