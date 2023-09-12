5b4e19143ac928cd8675b27db5273dd15b50b36a
Manoel Valladao <valladao@gmail.com>
2023-09-11 22:39:38 -0300

    Add hover to icon buttons

--- app/routes/database.expense-group.tsx
+++ app/routes/database.expense-group.tsx
@@ -22,22 +22,22 @@ export default function ExpenseGroup() {
               <td className="px-6 py-4 whitespace-nowrap">Expense Group 1</td>
               <td className="px-6 py-4 whitespace-nowrap">
                 <div className="flex space-x-2">
-                  <div className="rounded-md bg-emerald-600 p-2 pb-1">
+                  <div className="rounded-md bg-emerald-600 p-2 pb-1 hover:bg-emerald-700">
                     <button className="text-white">
                       <FaPenToSquare size={20} />
                     </button>
                   </div>
-                  <div className="rounded-md bg-red-600 p-2 pb-1">
+                  <div className="rounded-md bg-red-600 p-2 pb-1 hover:bg-red-700">
                     <button className="text-white">
                       <FaTrashCan size={20} />
                     </button>
                   </div>
-                  <div className="rounded-md bg-gray-600 p-2 pb-1">
+                  <div className="rounded-md bg-gray-600 p-2 pb-1 hover:bg-gray-700">
                     <button className="text-white">
                       <FaCaretDown size={20} />
                     </button>
                   </div>
-                  <div className="rounded-md bg-gray-600 p-2 pt-3 pb-0">
+                  <div className="rounded-md bg-gray-600 p-2 pt-3 pb-0 hover:bg-gray-700">
                     <button className="text-white">
                       <FaCaretUp size={20} />
                     </button>
