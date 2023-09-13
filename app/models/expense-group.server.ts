ccabeb1869acd93d4ca1930e84a1e6f8ce8f9465
Manoel Valladao <valladao@gmail.com>
2023-09-13 10:01:12 -0300

    Add getExpenseGroupItems and render information

--- app/models/expense-group.server.ts
+++ app/models/expense-group.server.ts
@@ -3,3 +3,7 @@ import prisma from "~/db.server"
 export async function createExpenseGroup(newEntry: any) {
   return prisma.expenseGroup.create({ data: newEntry })
 }
+
+export async function getExpenseGroupItems() {
+  return prisma.expenseGroup.findMany({})
+}
