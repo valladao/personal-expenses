74118c7d611937bc837712af0a4d57096678d331
Manoel Valladao <valladao@gmail.com>
2023-09-12 22:12:32 -0300

    Add hidden field checkbox

--- app/routes/database.expense-group.tsx
+++ app/routes/database.expense-group.tsx
@@ -20,14 +20,25 @@ export default function ExpenseGroup() {
       </div>
 
       <div className="w-1/2 p-4">
-        <h2 className="text-2xl font-bold mb-4">Edit Expense Group</h2>
+        <h2 className="text-2xl font-bold mb-4">Create New Expense Group</h2>
         <form>
           <div className="mb-4">
             <label htmlFor="name" className="block text-gray-700">Name:</label>
-            <input type="text" id="name" name="name" className="w-full px-4 py-2 border border-gray-300 rounded" />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full px-4 py-2 border border-gray-300 rounded"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="flex items-center space-x-2">
+              <input type="checkbox" className="form-checkbox h-5 w-5 text-emerald-600" />
+              <span className="text-gray-700">Hide Expense Group</span>
+            </label>
           </div>
           <div className="flex justify-end">
-            <button className="bg-emerald-600 hover:bg-emerald-700 text-white py-2 px-4 rounded">Save</button>
+            <button className="bg-emerald-600 hover:bg-emerald-700 text-white py-2 px-4 rounded">Create</button>
           </div>
         </form>
       </div>
