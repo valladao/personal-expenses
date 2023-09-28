b7540494c06df6c66b71e15a3737883f8cda021b
Manoel Valladao <valladao@gmail.com>
2023-09-28 12:29:54 -0300

    Create new sidebar click interactions

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -57,19 +57,32 @@ const basicSidebarLinks = [
 export type LinksListType = typeof databaseLinks;
 
 export default function Sidebar() {
+
+  const closeOnClick = (event: React.MouseEvent) => {
+    const target = event.target as HTMLElement | null;
+    if (target?.id == 'sidebar' || target?.classList.contains('sidebarLink')) {
+      const sidebar = document.getElementById('sidebar') as HTMLDialogElement | null;
+      const main = document.getElementById('main') as HTMLElement | null;
+      sidebar?.close()
+      main?.classList.remove('ml-64')
+    }
+  }
+
   return (
-    <dialog id="sidebar" className="w-64 bg-zinc-800 p-4 m-0 h-full max-h-fit">
-      <div className="flex items-center justify-between">
-        <Link to="/">
-          <h1 className="text-xl font-bold mr-4 text-zinc-100">Expenses App</h1>
-        </Link>
-        <ToggleSidebarButton text="&times;" />
+    <dialog id="sidebar" className="w-64 bg-zinc-800 p-0 m-0 h-full max-h-fit" onClick={closeOnClick}>
+      <div id="dialogBox" className="w-full h-full p-4">
+        <div className="flex items-center justify-between">
+          <Link to="/">
+            <h1 className="sidebarLink text-xl font-bold mr-4 text-zinc-100">Expenses App</h1>
+          </Link>
+          <ToggleSidebarButton text="&times;" />
+        </div>
+        {
+          basicSidebarLinks.map(linkList => {
+            return <LinkList key={linkList.listName} linkArray={linkList.linkArray} listName={linkList.listName}></LinkList>
+          })
+        }
       </div>
-      {
-        basicSidebarLinks.map(linkList => {
-          return <LinkList key={linkList.listName} linkArray={linkList.linkArray} listName={linkList.listName}></LinkList>
-        })
-      }
     </dialog>
   )
 }
